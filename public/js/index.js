4ca0a447c6e4c1adc54c928485014f908e019355
Mateusspadacio <mateusspadacio@gmail.com>
2018-07-24 14:59:18 -0300

    Greet new user

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,6 @@ socket.on('disconnect', function () {
     console.log('disconnected');
 });
 
-socket.on('newMessage', function(message) {
+socket.on('newMessage', function (message) {
     console.log(message);
 });
